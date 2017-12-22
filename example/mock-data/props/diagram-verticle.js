114ddac51bd19a14c1a0ea7d05e382c34d996c37
郭美山 <guomeishan@guomeishandeMacBook-Pro.local>
2017-12-22 18:58:19 +0800

    test ci - 1

---
+++ example/mock-data/props/diagram-verticle.js
@@ -0,0 +1,23 @@
+export default {
+  type: 'curveCardinal',
+  axisXBaseLine: true,
+  axisYBaseLine: true,
+  axisXLength: 5,
+  axisYLength: 5,
+  dataset: [
+    {
+      "label": "访问量",
+      "points": [
+        ["招聘", 30],
+        ["测评", 200],
+        ["平台", 100]
+      ],
+      "maxcolors": "#00ffff",
+      "mincolors": "#ff00ff"
+    }
+  ],
+  axisTitle: {
+    x: '',
+    y: ''
+  }
+}
---
+++ src/index.js
@@ -0,0 +1,69 @@
+/*
+ * @beisen/bs-chart v0.1.11
+ * (c) 2017 guomeishan
+ * Released under the ISC License.
+ * WARNING: The file is automatically generated, not manually modified. 
+ * 警告: 该文件为脚本自动生成，切勿手动修改 
+ */
+
+import chart_549ecd70e3a211e7b87ecdca7fd67a89 from "./components/charts/china-map/index.vue"
+import chart_549ecd71e3a211e7b87ecdca7fd67a89 from "./components/charts/data-grid-slider/index.vue"
+import chart_549ecd72e3a211e7b87ecdca7fd67a89 from "./components/charts/diagram/index.vue"
+import chart_549ecd73e3a211e7b87ecdca7fd67a89 from "./components/charts/histogram-v/index.vue"
+import chart_549ecd74e3a211e7b87ecdca7fd67a89 from "./components/charts/histogram/index.vue"
+import chart_549ecd75e3a211e7b87ecdca7fd67a89 from "./components/charts/pie/index.vue"
+import chart_549ecd76e3a211e7b87ecdca7fd67a89 from "./components/charts/text-printer/index.vue"
+import chart_549ecd77e3a211e7b87ecdca7fd67a89 from "./components/charts/text-printer/printer-item.vue"
+import chart_549ecd78e3a211e7b87ecdca7fd67a89 from "./components/charts/text-slider/index.vue"
+import chart_549ecd79e3a211e7b87ecdca7fd67a89 from "./components/charts/topological/index.vue"
+import chart_549ecd7ae3a211e7b87ecdca7fd67a89 from "./components/charts/world-map/index.vue"
+import chart_549ecd7be3a211e7b87ecdca7fd67a89 from "./components/fragment/number-with-bg/index.vue"
+import chart_549ecd7ce3a211e7b87ecdca7fd67a89 from "./components/fragment/number/index.vue"
+import chart_549ecd7de3a211e7b87ecdca7fd67a89 from "./components/fragment/scale-tool/index.vue"
+import chart_549ecd7ee3a211e7b87ecdca7fd67a89 from "./components/layouts/app-root/index.vue"
+import chart_549ecd7fe3a211e7b87ecdca7fd67a89 from "./components/layouts/china-map-layout/index.vue"
+import chart_549ecd80e3a211e7b87ecdca7fd67a89 from "./components/layouts/date-tool/index.vue"
+import chart_549ecd81e3a211e7b87ecdca7fd67a89 from "./components/layouts/header/index.vue"
+import chart_549ecd82e3a211e7b87ecdca7fd67a89 from "./components/layouts/layout-card/index.vue"
+import chart_549ecd83e3a211e7b87ecdca7fd67a89 from "./components/layouts/page-footer/index.vue"
+import chart_549ecd84e3a211e7b87ecdca7fd67a89 from "./components/layouts/page-layout/index.vue"
+import chart_549ecd85e3a211e7b87ecdca7fd67a89 from "./components/layouts/world-map-layout/index.vue"
+import formattings from "@utils/formattings"
+require("@asserts/styles/all.css")
+
+  let BSCharts = {
+    components: {},
+    formattings: formattings,
+    version: '0.1.11'
+  }
+
+BSCharts.components[chart_549ecd70e3a211e7b87ecdca7fd67a89.name] = chart_549ecd70e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd71e3a211e7b87ecdca7fd67a89.name] = chart_549ecd71e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd72e3a211e7b87ecdca7fd67a89.name] = chart_549ecd72e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd73e3a211e7b87ecdca7fd67a89.name] = chart_549ecd73e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd74e3a211e7b87ecdca7fd67a89.name] = chart_549ecd74e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd75e3a211e7b87ecdca7fd67a89.name] = chart_549ecd75e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd76e3a211e7b87ecdca7fd67a89.name] = chart_549ecd76e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd77e3a211e7b87ecdca7fd67a89.name] = chart_549ecd77e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd78e3a211e7b87ecdca7fd67a89.name] = chart_549ecd78e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd79e3a211e7b87ecdca7fd67a89.name] = chart_549ecd79e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7ae3a211e7b87ecdca7fd67a89.name] = chart_549ecd7ae3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7be3a211e7b87ecdca7fd67a89.name] = chart_549ecd7be3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7ce3a211e7b87ecdca7fd67a89.name] = chart_549ecd7ce3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7de3a211e7b87ecdca7fd67a89.name] = chart_549ecd7de3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7ee3a211e7b87ecdca7fd67a89.name] = chart_549ecd7ee3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd7fe3a211e7b87ecdca7fd67a89.name] = chart_549ecd7fe3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd80e3a211e7b87ecdca7fd67a89.name] = chart_549ecd80e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd81e3a211e7b87ecdca7fd67a89.name] = chart_549ecd81e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd82e3a211e7b87ecdca7fd67a89.name] = chart_549ecd82e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd83e3a211e7b87ecdca7fd67a89.name] = chart_549ecd83e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd84e3a211e7b87ecdca7fd67a89.name] = chart_549ecd84e3a211e7b87ecdca7fd67a89
+BSCharts.components[chart_549ecd85e3a211e7b87ecdca7fd67a89.name] = chart_549ecd85e3a211e7b87ecdca7fd67a89
+
+  BSCharts.install = function (Vue, options = {}) {
+    Object.keys(this.components).forEach(name => {
+      Vue.component(name, this.components[name])
+    })
+  }
+
+export default BSCharts
